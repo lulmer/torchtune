32b0b20e700fd2ff1c2f33eaebcd4eb28398b5d0
lulmer <ulmerlouis@gmail.com>
2024-07-02 23:50:44 +0000

    fixing signatures and forwards methods, adding a Gemma2DecoderLayer

--- torchtune/models/gemma2/__init__.py
+++ torchtune/models/gemma2/__init__.py
@@ -4,14 +4,10 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from ._component_builders import gemma  # noqa
+from ._component_builders import gemma2  # noqa
 from ._convert_weights import gemma_hf_to_tune, gemma_tune_to_hf  # noqa
 from ._model_builders import (  # noqa
-    gemma_2b,
-    gemma_7b,
+    gemma2_9b,
+    gemma2_27b,
     gemma_tokenizer,
-    lora_gemma_2b,
-    lora_gemma_7b,
-    qlora_gemma_2b,
-    qlora_gemma_7b,
 )
